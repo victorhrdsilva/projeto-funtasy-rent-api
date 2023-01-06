003801a435ea553fe770dfa8893e60f2cd27729a
victorhrdsilva <victorhrdsilva@gmail.com>
2023-01-06 18:22:35 -0300

    fix db

--- src/db/db.js
+++ src/db/db.js
@@ -5,14 +5,6 @@ dotenv.config();
 
 const { Pool } = pg;
 
-/* const connection = new Pool({
-  user: 'postgres',
-  password: '310799',
-  host: 'localhost',
-  port: 5432,
-  database: 'funtasy-rent'
-}); */
-console.log(process.env.DATABASE_URL)
 const connection = new Pool({
     connectionString: process.env.DATABASE_URL,
 });
